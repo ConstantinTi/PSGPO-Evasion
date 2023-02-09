a1d8e9a6f4744d16a8f03e66dd07427583427685
ConstantinTi <54210903+ConstantinTi@users.noreply.github.com>
2023-02-09 08:36:41 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Management.Automation;
+
+namespace PSGPO_Evasion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerShell ps = PowerShell.Create();
+            string command;
+            Collection<PSObject> PSOutput;
+            while (true)
+            {
+                command = Console.ReadLine();
+                PSOutput = ps.AddScript(command).Invoke();
+
+                if (command == "exit")
+                {
+                    break;
+                }
+                for (int i = 0; i < PSOutput.Count; i++)
+                {
+                    Console.WriteLine(PSOutput[i].ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
